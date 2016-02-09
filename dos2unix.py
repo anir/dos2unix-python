c55dbc35acc67fc46ef3941d0a04c57796c466fa
anir <anirban25987@gmail.com>
2016-02-09 00:48:02 +0000

    stub code for dos2unix

---
+++ dos2unix.py
@@ -0,0 +1,13 @@
+import sys
+filename = sys.argv[2]
+operation = sys.argv[1]
+
+if operation == 'dos2unix':
+    text = open(filename, 'rb').read().replace('\r\n', '\n')
+    open(filename, 'wb').write(text)
+elif operation == 'unix2dos':
+    text = open(filename, 'rb').read().replace('\n', '\r\n')
+    open(filename, 'wb').write(text)
+else:
+    sys.exit('Possible args can be dos2unix or unix2dos')
+
